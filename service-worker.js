a9d984c88121680d14547052c3bbd6213d644dd3
tramax73 <tramax73@gmail.com>
2020-05-03 00:07:04 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+var cacheName = 'verbos-pwa';
+var filesToCache = [
+  '/',
+  '/verbos-pwa.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/js/funcs.js',
+  '/js/json-data.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
