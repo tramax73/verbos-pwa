0f68faad10594811c79162189f2b050217456184
tramax73 <tramax73@gmail.com>
2020-05-15 23:24:05 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = () => {
   'use strict';
 
   if('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('./service-worker.js')
         .then(registration => {
           console.log("Service Worker Registered");
 
