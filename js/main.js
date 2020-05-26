93089379548fd2abe4a5b47689371c72224d65f9
tramax73 <tramax73@gmail.com>
2020-05-26 16:51:47 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 window.onload = () => {
   'use strict';
 
+  let installingWorker;
+
+  // The click event on the notification
+  document.getElementById('reload').addEventListener('click', function(){
+    installingWorker.postMessage({ action: 'skipWaiting' });
+  });
+
   if('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./service-worker.js')
         .then(registration => {
@@ -13,8 +20,6 @@ window.onload = () => {
             registration.addEventListener('updatefound', () => {
               console.log('Update found. Waiting for install to complete.');
               const installingWorker = registration.installing;
-
-
 
               // Watch for changes to the worker's state. Once it is "installed", our cache
               // has been updated with our new files, so we can prompt the user to instantly
