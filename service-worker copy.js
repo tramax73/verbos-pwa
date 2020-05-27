e8755badc6af59c18ff0fb08c6a3d8bc211c459f
tramax73 <tramax73@gmail.com>
2020-05-27 08:40:49 +0200

    candidate 1.3
    remove main.js
    
    updated service worker.js

--- service-worker copy.js
+++ service-worker copy.js
@@ -1,20 +1,29 @@
 var cacheName = 'verbos-pwa';
-
 var filesToCache = [
   '/verbos-pwa/',
   '/verbos-pwa/manifest.json',
   '/verbos-pwa/index.html',
   '/verbos-pwa/css/style.css',
-  // '/verbos-pwa/js/main.js',
+  '/verbos-pwa/js/main.js',
   '/verbos-pwa/js/funcs.js',
   '/verbos-pwa/js/json-data.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(filesToCache))
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
 
@@ -24,21 +33,6 @@ self.addEventListener('message', function (event) {
     self.skipWaiting();
   }
 });
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-
 
 // VERSION: 1.2
 // BUILD: 134
